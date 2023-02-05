22920a886d0f80aa7517ecaeed07a99f13b23eab
Suriya <suriyaganesh097@gmail.com>
2023-02-05 19:08:37 +0530

    Text editor quits with ^q and is able to run cleanly in raw mode

--- main.cpp
+++ main.cpp
@@ -2,32 +2,51 @@
 #include <unistd.h>
 #include <termios.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 
 struct termios orig_termios;
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1){
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
 
-    tcgetattr(STDIN_FILENO, &orig_termios); //get terminal attributes
-
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON); //Flip the ECHO and ICANON bits in c_flag
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //Set the new terminal setting
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr"); //Set the new terminal setting
+
 }
+
 
 int main() {
     enableRawMode();
 
-    char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while(1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\n", c);
         } else {
             printf("%d ('%c')\n", c, c);
         }
+        if (c == CTRL_KEY('q')) break;
     };
     return 0;
 }
