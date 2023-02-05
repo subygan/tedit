45d0cb5326f6ab5d3f8634b4aa6d5f2973a242fc
Suriya <suriyaganesh097@gmail.com>
2023-02-05 17:11:00 +0530

    Working implementation of Raw mode text editor with canonical mode turned off. this means that, every character is being read by the program, character by character.

---
+++ README.md
@@ -0,0 +1,3 @@
+### Building a Text editor in 1K lines of c++
+
+Based on [Antirez's kilo](http://antirez.com/news/108) and [kilo](https://viewsourcecode.org/snaptoken/kilo/index.html)
--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <unistd.h>
+#include <termios.h>
+
+
+struct termios orig_termios;
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+
+    tcgetattr(STDIN_FILENO, &orig_termios); //get terminal attributes
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON); //Flip the ECHO and ICANON bits in c_flag
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); //Set the new terminal setting
+}
 
 int main() {
-    // Write C++ code here
-    std::cout << "Hello world!";
+    enableRawMode();
+
     char c;
-    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    };
     return 0;
 }
